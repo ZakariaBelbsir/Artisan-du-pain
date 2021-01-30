f5cf10b77e5bd96fc77786503dc176241444bfb1
ZakariaBelbsir <zakaria.belbsir@gmail.com>
2021-01-30 18:39:08 +0100

    form submission 5

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,9 +5,9 @@
     </div>
     <div class="container my-12 flex content-center items-center">
       <form class="max-w-lg w-full mx-auto border shadow-xl rounded-lg px-4 py-2"
-            method="POST" data-netlify="true"
+            method="POST" netlify netlify-honeypot="bot-field"
             name="contact"
-            data-netlify-honeypot="bot-field">
+            hidden>
         <input type="hidden" name="form-name" value="contact" />
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
