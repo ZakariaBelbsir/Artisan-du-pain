70e53da015e0caf9776f3b79417605e0d4f8f12b
ZakariaBelbsir <zakaria.belbsir@gmail.com>
2021-01-30 17:20:25 +0100

    form submission 2

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,39 +5,44 @@
     </div>
     <div class="container my-12 flex content-center items-center">
       <form class="max-w-lg w-full mx-auto border shadow-xl rounded-lg px-4 py-2"
-            method="POST" data-netlify="true">
+            method="POST" data-netlify="true"
+            data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="ask-question" />
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
+            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="name">
               Nom
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
            border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
            focus:bg-white focus:border-gray-500"
-                   id="name" required type="text">
+                   id="name" required type="text"
+            name="name">
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
+            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="email">
               E-mail
             </label>
             <input
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border
               border-gray-200 rounded py-3 px-4 mb-3 leading-tight
               focus:outline-none focus:bg-white focus:border-gray-500"
-                id="email" required type="email">
+                id="email" required type="email"
+            name="email">
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="grid-password">
+            <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" for="message">
               Message
             </label>
             <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border
            border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
            focus:bg-white focus:border-gray-500 h-48 resize-none"
-                      required id="message"></textarea>
+                      required id="message"
+            name="message"></textarea>
           </div>
         </div>
         <div class="">
